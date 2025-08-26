3d8bed246f883a4ec597a287ce29dc48bccc3aaa
ASADHASSAN <cybersecjunior00@gmail.com>
2025-08-26 17:15:54 +0100

    ad 2 ec2 instances 1 public and 1 private

--- main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "app_server" {
-  ami           = "ami-0cfb394ad3c3ac699"
-  instance_type = "t3.nano"
-
-  tags = {
-    Name   = "TerraformAppServer"
-    Owner  = "ASAD"
-  }
-}
