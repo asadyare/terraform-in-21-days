dc450dcde6b2ab2124ea6147da54d22ec25b5859
ASADHASSAN <cybersecjunior00@gmail.com>
2025-09-06 08:51:56 +0100

    Configure remote state s3

--- provider.tf
+++ provider.tf
@@ -1,4 +1,12 @@
 # Configure the AWS Provider
+
+terraform {
+  backend "s3" {
+    bucket         = "min-terraform-remote-state"
+    key            = "terraform.tfstate"
+    region        = "eu-west-2"
+  }
+}
 provider "aws" {
   region = "eu-west-2"
 }
