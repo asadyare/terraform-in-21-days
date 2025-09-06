cda62b203ff2e406fa7b6613d6a5af233c09db7f
ASADHASSAN <cybersecjunior00@gmail.com>
2025-09-06 08:28:05 +0100

    All good

--- instances.tf
+++ instances.tf
@@ -1,99 +1,99 @@
-# data "aws_ami" "amazonlinux"{
-#   most_recent = true
-#   owners      = ["amazon"]
+data "aws_ami" "amazonlinux"{
+  most_recent = true
+  owners      = ["amazon"]
 
-#   filter {
+  filter {
     
-#     name     = "name"
-#     values   = ["amzn2-ami-kernel-*"]
-#   }
+    name     = "name"
+    values   = ["amzn2-ami-kernel-*"]
+  }
 
-#   filter {
-#     name   = "virtualization-type"
-#     values = ["hvm"]
-#   }
-# }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
 
-# resource "aws_instance" "public" {
-#   ami                         = data.aws_ami.amazonlinux.id
-#   associate_public_ip_address = true
-#   instance_type               = "t3.micro"
-#   key_name                    = "warsan"
-#   vpc_security_group_ids      = [aws_security_group.public.id]
-#   subnet_id                   = aws_subnet.public[0].id
-#   user_data                   = file("user-data.sh")
+resource "aws_instance" "public" {
+  ami                         = data.aws_ami.amazonlinux.id
+  associate_public_ip_address = true
+  instance_type               = "t3.micro"
+  key_name                    = "warsan"
+  vpc_security_group_ids      = [aws_security_group.public.id]
+  subnet_id                   = aws_subnet.public[0].id
+  user_data                   = file("user-data.sh")
 
-#   tags = {
-#     Name = "${var.env_code}-Public"
-#   }
-# }
+  tags = {
+    Name = "${var.env_code}-Public"
+  }
+}
 
-# resource "aws_security_group" "public" {
-#   name        = "${var.env_code}-Public"
-#   description = "Allow Inbound traffic"
-#   vpc_id      = aws_vpc.main.id
+resource "aws_security_group" "public" {
+  name        = "${var.env_code}-Public"
+  description = "Allow Inbound traffic"
+  vpc_id      = aws_vpc.main.id
 
-#   ingress {
-#     description = "SSH from public"
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = ["82.42.201.216/32"]
-#   }
+  ingress {
+    description = "SSH from public"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["82.42.201.216/32"]
+  }
 
-#   ingress {
-#     description = "HTTP from public"
-#     from_port   = 80
-#     to_port     = 80
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  ingress {
+    description = "HTTP from public"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   tags = {
-#     Name = "${var.env_code}-Public"
-#   }
-# }
+  tags = {
+    Name = "${var.env_code}-Public"
+  }
+}
 
-# resource "aws_instance" "private" {
-#   ami                    = data.aws_ami.amazonlinux.id
-#   instance_type          = "t3.micro"
-#   key_name               = "warsan"
-#   vpc_security_group_ids = [aws_security_group.private.id]
-#   subnet_id              = aws_subnet.private[0].id
+resource "aws_instance" "private" {
+  ami                    = data.aws_ami.amazonlinux.id
+  instance_type          = "t3.micro"
+  key_name               = "warsan"
+  vpc_security_group_ids = [aws_security_group.private.id]
+  subnet_id              = aws_subnet.private[0].id
 
-#   tags = {
-#     Name = "${var.env_code}-Private"
-#   }
-# }
+  tags = {
+    Name = "${var.env_code}-Private"
+  }
+}
 
-# resource "aws_security_group" "private" {
-#   name        = "${var.env_code}-Private"
-#   description = "Allow vpc traffic"
-#   vpc_id      = aws_vpc.main.id
+resource "aws_security_group" "private" {
+  name        = "${var.env_code}-Private"
+  description = "Allow vpc traffic"
+  vpc_id      = aws_vpc.main.id
 
-#   ingress {
-#     description = "SSH from vpc"
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = var.private_cidr
-#   }
+  ingress {
+    description = "SSH from vpc"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.private_cidr
+  }
 
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   tags = {
-#     Name = "${var.env_code}-Private"
-#   }
-# }
+  tags = {
+    Name = "${var.env_code}-Private"
+  }
+}
