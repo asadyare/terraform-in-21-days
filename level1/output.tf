f4dd41d9c191010b02a8aec9ec24724d95599c3f
ASADHASSAN <cybersecjunior00@gmail.com>
2025-09-06 17:17:12 +0100

    Split terraform state into multible layers

---
+++ level1/output.tf
@@ -0,0 +1,19 @@
+output "public_subnet_ids" {
+  description = "The IDs of the public subnets"
+  value       = aws_subnet.public.*.id
+  
+}
+
+output "private_subnet_ids" {
+  description = "The IDs of the private subnets"
+  value       = aws_subnet.private.*.id
+}
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "vpc_cidr" {
+  description = "The CIDR block of the VPC"
+  value       = var.vpc_cidr
+}
