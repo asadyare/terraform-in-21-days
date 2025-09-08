defb3ce3055903767a8235b0c6b9ac74b5593240
ASADHASSAN <cybersecjunior00@gmail.com>
2025-09-08 09:58:04 +0100

    Configure Auto Scaling Groups

--- level2/instances.tf
+++ level2/instances.tf
@@ -111,7 +111,7 @@ resource "aws_autoscaling_group" "main" {
   name                 = "${var.env_code}-AutoScaling"
   desired_capacity     = 2
   max_size             = 2
-  min_size             = 4
+  min_size             = 2
 
   target_group_arns = [aws_lb_target_group.main.arn]
   vpc_zone_identifier  = try(data.terraform_remote_state.level1.outputs["public_subnet_ids"], null)
