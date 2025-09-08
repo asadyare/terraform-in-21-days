16442bf073708b132bc4114b2ffbb8fef2081a93
ASADHASSAN <cybersecjunior00@gmail.com>
2025-09-08 19:53:49 +0100

    adding iam role for our instance

---
+++ level2/iam.tf
@@ -0,0 +1,23 @@
+resource "aws_iam_role" "main" {
+  name = var.env_code
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm" {
+  role       = aws_iam_role.main.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
+}
+
+resource "aws_iam_instance_profile" "main" {
+  name = var.env_code
+  role = aws_iam_role.main.name
+}
