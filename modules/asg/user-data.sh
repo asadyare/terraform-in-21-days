a1ae8cd84c888bf7a3f684841ee5bae42d44f229
ASADHASSAN <cybersecjunior00@gmail.com>
2025-09-28 17:19:32 +0100

    tried to create open source modules something is wrong but will fix later

--- modules/asg/user-data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-yum update -y
-yum install -y httpd git
-# git clone https://github.com/gabrielecirulli/2048.git
-# cp -R 2048/* /var/www/html/
-echo "<h1>Hello from $(hostname -f) by Asad</h1>" > /var/www/html/index.html
-systemctl start httpd && systemctl enable httpd
